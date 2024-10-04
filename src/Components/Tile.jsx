e4dbec15d1d8efc055a8ae905388a71e97538536
KalilahClarke <kalilah.clarke@gmail.com>
2024-10-03 22:04:16 -0400

    Hover functional

--- src/Components/Tile.jsx
+++ src/Components/Tile.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 export default function Tile({
   index,
@@ -6,21 +6,32 @@ export default function Tile({
   tiles,
   setTiles,
   player,
-  setPlayer,
-}) {
+  setPlayer
+}){
+  const [hover, setHover] = useState('');
+
+  useEffect(() => {
+    setHover(player === 'X' ? 'x-hover' : 'o-hover');
+  }, [player]);
+
   const handleTileClick = () => {
-    if (tiles[index] !== null) {
-      return;
-    }
+
+    if (tiles[index] !== null) return;
 
     const newTiles = [...tiles];
     newTiles[index] = player;
     setTiles(newTiles);
     setPlayer(player === "X" ? "O" : "X");
+
   };
+
   return (
-    <div className="tile" onClick={handleTileClick}>
-      {value}
+    <div 
+      id={`${index}`}
+      className={`tile ${value ? '' : hover}`} 
+      onClick={handleTileClick}
+    >
+      { value }
     </div>
   );
 }
